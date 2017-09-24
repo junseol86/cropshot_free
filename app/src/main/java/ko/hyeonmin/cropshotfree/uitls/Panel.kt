f7caa739dad202a9605324177c6089337abe7b52
DESKTOP-TVVDN94\junse <junseol86@gmail.com>
2017-09-24 12:19:35 +0900

    ADD Folder button ontouchlistener

--- app/src/main/java/ko/hyeonmin/cropshotfree/uitls/Panel.kt
+++ app/src/main/java/ko/hyeonmin/cropshotfree/uitls/Panel.kt
@@ -1,9 +1,12 @@
 package ko.hyeonmin.cropshotfree.uitls
 
+import android.graphics.Color
 import android.support.constraint.ConstraintLayout
+import android.view.MotionEvent
 import android.widget.TextView
 import ko.hyeonmin.cropshotfree.CropShotActivity
 import ko.hyeonmin.cropshotfree.R
+import java.io.File
 
 /**
  * Created by Hyeonmin on 2017-09-10.
@@ -19,5 +22,17 @@ class Panel(activity: CropShotActivity) {
 
         folderNameTV = activity.findViewById(R.id.folder_name) as TextView
         folderNameTV?.text = activity.caches!!.folderName
+
+        panelCl?.setOnTouchListener { _, event ->
+            when (event!!.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    panelCl?.setBackgroundColor(Color.parseColor("#333333"))
+                }
+                MotionEvent.ACTION_UP -> {
+                    panelCl?.setBackgroundColor(Color.parseColor("#111111"))
+                }
+            }
+            false
+        }
     }
 }
